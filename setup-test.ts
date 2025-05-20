b9f88ceeadfc9529eec25cd49c167dc62c85159f
Ryan Porterfield <ixirsii@ixirsii.tech>
2025-05-20 15:00:41 -0700

    fix(tests): Mock matchMedia function for next-themes

--- setup-test.ts
+++ setup-test.ts
@@ -1,1 +1,18 @@
 import "@testing-library/jest-dom/vitest";
+
+import { vi } from "vitest";
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});

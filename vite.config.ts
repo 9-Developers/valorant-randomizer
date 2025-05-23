54edd12c945a53679c312918891666946f515b94
Ryan Porterfield <ixirsii@ixirsii.tech>
2025-05-23 16:06:40 -0500

    feat(theme-test): Add theme switcher tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
+    coverage: {
+      exclude: ["*.config.[jt]s", "**/env.js", ...coverageConfigDefaults.exclude],
+    },
     environment: "jsdom",
     setupFiles: ["setup-test.ts"],
   },
